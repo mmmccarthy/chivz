75bf33fd03f9229894560c706ffed7f25004c92d
Michael McCarthy <mmcc93@gmail.com>
2020-10-11 23:24:28 -0500

    Summarize crashes by intersection, fix IDOT Crash Types, dates

--- crash_summaries/intersections.R
+++ crash_summaries/intersections.R
@@ -4,11 +4,13 @@
 # Merge IDOT and Chicago crash records
 idot = readRDS("../idot_crashes/IDOT_Crashes_Chicago_2009_2017.rds")
 chicago = readRDS("../chicago_crashes/Crashes_2018_2019.rds")
+chicago$crash_date = as.character(chicago$crash_date) # weird date shenanigans
 crashes = rbind(idot,chicago)
 
 # Create SF and Project
 crashes = crashes %>%
-  filter(latitude > 35 & longitude < -85)
+  filter(latitude > 35 & longitude < -85) %>%
+  filter(first_crash_type %in% c("PEDESTRIAN","PEDALCYCLIST"))
 crashes = st_as_sf(crashes, coords = c("longitude","latitude"))
 crashes = st_set_crs(crashes, 4326)
 
@@ -23,14 +25,15 @@ crashes_projected = st_transform(crashes,crs = 3435)
 
 # Intersect crashes with buffer / handle overlapping buffers
 crash_by_intersection = st_join(crashes_projected,chi_intersections_buffer, join = st_within, largest = TRUE)
-crashes$intersection = ifelse(!is.na(crash_by_intersection$STREET_NAM),paste0(str_to_title(crash_by_intersection$STREET_NAM)," / ",str_to_title(crash_by_intersection$STREET_NAM_2)),NA)
+crashes$intersection = ifelse(!is.na(crash_by_intersection$street_nam) & !is.na(crash_by_intersection$street_nam_2),paste0(str_to_title(crash_by_intersection$street_nam)," / ",str_to_title(crash_by_intersection$street_nam_2)),NA)
 
 # Summarize crashes by Intersection
+
 intersection_summary = crashes %>%
   st_drop_geometry() %>%
-  filter(first_crash_type %in% c("PEDESTRIAN", "PEDALCYCLIST", "Pedestrian", "Pedalcyclist") & !is.na(most_severe_injury)) %>% # Data portal has some non-injury crashes where all of the summary fields are NA
+  filter(!is.na(intersection)) %>%
   mutate(crash_date = as.POSIXct(crash_date), year = format(crash_date,"%Y")) %>%
-  group_by(intersection, year) %>%
+  group_by(intersection, first_crash_type, year) %>%
   summarize(crashes = n(), injuries_total = sum(injuries_total), injuries_fatal = sum(injuries_fatal), injuries_incapacitating = sum(injuries_incapacitating), injuries_non_incapacitating = sum(injuries_non_incapacitating), injuries_reported_not_evident = sum(injuries_reported_not_evident))
 
 # Save results
